8206ff1f6f7bf81532e47d39cc3030f4622b4cc9
LeonAdeoye <leon.adeoye@gmail.com>
2020-05-13 23:42:40 +0800

    added component

---
+++ src/app/components/usage/usage.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-usage",
+  templateUrl: "./usage.component.html",
+  styleUrls: ["./usage.component.sass"]
+})
+export class UsageComponent implements OnInit
+{
+
+  constructor() { }
+
+  ngOnInit(): void
+  {
+  }
+}
