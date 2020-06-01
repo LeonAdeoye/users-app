57d978a5c4f0d7102163ea8abed48f5b962c222f
LeonAdeoye <leon.adeoye@gmail.com>
2020-06-01 23:24:14 +0800

    fixed issue with app usage count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainGridComponent } from './components/main-grid/main-grid.component';
-import { MainHeaderComponent } from './components/main-header/main-header.component';
-import { AgGridModule } from 'ag-grid-angular';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MainGridComponent } from "./components/main-grid/main-grid.component";
+import { MainHeaderComponent } from "./components/main-header/main-header.component";
+import { AgGridModule } from "ag-grid-angular";
 import { LoggingService } from "./services/logging.service";
 import { MessageService } from "./services/message.service";
 import { ConfigurationService } from "./services/configuration.service";
 import { BootstrapService } from "./services/bootstrap.service";
 import { PopupService } from "./services/popup.service";
 import { HttpClientModule } from "@angular/common/http";
-import { MaterialModule } from './material.module';
+import { MaterialModule } from "./material.module";
 import { FormsModule } from "@angular/forms";
 import { DetailComponent } from "./components/detail/detail.component";
 import { UsersComponent } from "./components/users/users.component";
 import { UsageComponent } from "./components/usage/usage.component";
+import { AppUsageComponent } from "./components/app-usage/app-usage.component";
 
 
 @NgModule({
@@ -33,7 +34,9 @@ import { UsageComponent } from "./components/usage/usage.component";
   ],
   imports: [
     BrowserModule,
-    AgGridModule.withComponents([]),
+    AgGridModule.withComponents([
+      AppUsageComponent
+    ]),
     BrowserAnimationsModule,
     HttpClientModule,
     MaterialModule,
--- src/app/components/app-usage/app-usage.component.ts
+++ src/app/components/app-usage/app-usage.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from "@angular/core";
+import { IAfterGuiAttachedParams, ICellRendererComp, ICellRendererParams } from "ag-grid-community";
 
 @Component({
   selector: "app-app-usage",
   templateUrl: "./app-usage.component.html",
   styleUrls: ["./app-usage.component.sass"]
 })
-export class AppUsageComponent implements OnInit
+export class AppUsageComponent implements ICellRendererComp
 {
 
   constructor() { }
 
-  ngOnInit(): void {
+  afterGuiAttached(params?: IAfterGuiAttachedParams): void
+  {
+  }
+
+  destroy(): void
+  {
+  }
+
+  getGui(): HTMLElement
+  {
+    return undefined;
+  }
+
+  init(params: ICellRendererParams): Promise<void> | void
+  {
+    return undefined;
+  }
+
+  refresh(params: any): boolean
+  {
+    return false;
   }
 
 }
