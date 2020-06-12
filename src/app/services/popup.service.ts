f435efbc97fea61513dc42b82bdd34c21b9e2c5f
LeonAdeoye <leon.adeoye@gmail.com>
2020-06-12 23:10:49 +0800

    add popup window

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { Subject } from "rxjs";
+import { Answer } from "../models/types";
+import { PopupComponent } from "../components/popup/popup.component";
+import { take } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PopupService
 {
-  constructor() { }
+  private bsModalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+
+  show(isErrorPopupFlag: boolean, titleToDisplay: string, messageToDisplay: string, ok?: boolean, cancel?: boolean, yes?: boolean, no?: boolean): Subject<Answer>
+  {
+    const windowProperties =
+    {
+      width: 400,
+      message: messageToDisplay,
+      title: titleToDisplay,
+      isErrorPopup: isErrorPopupFlag,
+      class: "modal-dialog modal-sm"
+    };
+
+    this.bsModalRef = this.modalService.show(PopupComponent, windowProperties);
+    this.initialise(yes, no, ok, cancel);
+    return this.bsModalRef.content.answerSubject.pipe(take(1));
+  }
+
+  private initialise(yes: boolean, no: boolean, ok: boolean, cancel: boolean): void
+  {
+    this.bsModalRef.content.answerSubject = new Subject<boolean>();
+
+    if(yes)
+      this.bsModalRef.content.showYesButton = true;
+
+    if(no)
+      this.bsModalRef.content.showNoButton = true;
+
+    if(cancel)
+      this.bsModalRef.content.showCancelButton = true;
+
+    if(ok)
+      this.bsModalRef.content.showOkButton = true;
+  }
 }
