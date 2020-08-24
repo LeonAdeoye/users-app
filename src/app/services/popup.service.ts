7d8c0b34dbda3ee2803a20b144b058b571cad039
LeonAdeoye <leon.adeoye@gmail.com>
2020-08-24 22:51:34 +0800

    center popup window

--- src/app/services/popup.service.ts
+++ src/app/services/popup.service.ts
@@ -37,7 +37,7 @@ export class PopupService
       class: "modal-lg"
     };
 
-    this.bsModalRef = this.modalService.show(DeskUsageComponent, { initialState, keyboard: false, backdrop: true });
+    this.bsModalRef = this.modalService.show(DeskUsageComponent, { class: "modal-dialog-centered", initialState, keyboard: false, backdrop: true });
     this.bsModalRef.content.title = "Desk usage for " + deskName;
     this.bsModalRef.content.deskDrilldown = deskName;
     this.initialise(true, true, true, true);
