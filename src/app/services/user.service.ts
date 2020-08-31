410ce989074681463e2b7d196ae91f62ba977ca7
LeonAdeoye <leon.adeoye@gmail.com>
2020-08-31 14:21:52 +0800

    fixed saving

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -74,7 +74,7 @@ export class UserService
   public saveUser(user: User): void
   {
     this.log(`Saving user: ${user}`, LogLevel.DEBUG);
-    const message = new Message(`${this.usersServiceURLBase}/user`, user.toJSON(), MessageTransport.HTTP, UtilityService.isNullOrEmptyOrBlankOrUndefined(user.userId) ? MessageMethod.POST : MessageMethod.PUT);
+    const message = new Message(`${this.usersServiceURLBase}/user`, user.toJSON(), MessageTransport.HTTP, UtilityService.isNullOrEmptyOrBlankOrUndefined(user.id) ? MessageMethod.POST : MessageMethod.PUT);
 
     this.messageService.send(message).subscribe(
       (result) =>
